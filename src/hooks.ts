556eaff0a87fa084a867b6225276299a6209d16b
Hirbod Mirjavadi <hm@nightstomp.com>
2022-06-06 08:36:50 +0200

    feat: add useSilentSwitch hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,5 +1,10 @@
 import { useState, useEffect } from 'react';
-import { getRingerMode, setRingerMode } from './native';
+import {
+  addSilentListener,
+  getRingerMode,
+  setNativeSilenceCheckInterval,
+  setRingerMode,
+} from './native';
 import type { RingerModeType } from './types';
 
 /**
@@ -36,4 +41,32 @@ export const useRingerMode = () => {
   };
 
   return { mode, error, setMode };
+};
+
+/**
+ * A hook to check if the iOS device is silent. (Silent switch status)
+ * @platform iOS
+ * @param {number} nativeIntervalCheck The native interval to check the status in seconds. 0.5, default 2.
+ * @returns boolean on iOS, undefined on other platforms
+ * @example
+ * ```ts
+  const isSilent = useSilentSwitch(nativeIntervalCheck?: number);
+ * ```
+ */
+export const useSilentSwitch = (nativeIntervalCheck?: number) => {
+  const [isSilent, setIsSilent] = useState<boolean | undefined>();
+
+  if (nativeIntervalCheck) setNativeSilenceCheckInterval(nativeIntervalCheck);
+
+  useEffect(() => {
+    const silentListener = addSilentListener((status) => {
+      setIsSilent(status);
+    });
+
+    return function unmountSilentSwitchListener() {
+      silentListener.remove();
+    };
+  }, []);
+
+  return isSilent;
 };
